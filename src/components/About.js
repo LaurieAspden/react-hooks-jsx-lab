3cc97ee86af8968832e953b9ab7700c05bb1a287
LaurieAspden <aspdenlaurie@gmail.com>
2021-12-27 18:40:00 +0800

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am Laurie, I am learning how to build websites and apps with JavaScript and React. 
+      I decided this was going to be my next challenge because I want to work from anywhere in the world, doing what I love. 
+      I am ready to develop my skills to the next level, keep learning and evolving. 
+    </p>
+    <img src={ image } alt="I made this" /> 
+  </div>
+  }
 
 export default About;
